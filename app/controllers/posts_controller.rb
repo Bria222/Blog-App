789b7c88f459fbea0b445b104b018bfd17832b73
Bria222 <techconsultants.bree@gmail.com>
2022-11-03 11:49:06 +0300

    add method to destroy post and comment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:comments)
+    @current_user = current_user.id
   end
 
   def show
@@ -11,6 +13,7 @@ class PostsController < ApplicationController
   end
 
   def new
+    @user = current_user
     @post = Post.new
   end
 
@@ -18,7 +21,6 @@ class PostsController < ApplicationController
     @post = current_user.posts.new(post_params)
     @post.likescounter = 0
     @post.commentscounter = 0
-    @post.update_post_counter
 
     respond_to do |format|
       format.html do
@@ -26,11 +28,22 @@ class PostsController < ApplicationController
           flash[:success] = 'Post saved successfully'
           redirect_to user_post_path(current_user, @post.id)
         else
-          flash.now[:error] = 'Error: Post could not be saved. Please try again.'
+          flash.now[:error] = 'Error: Please try again. Post could not be saved. '
           render :new
         end
       end
     end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    user = User.find(post.user_id)
+    user.postscounter -= 1
+    post.destroy
+    user.save
+    flash[:success] = 'You have deleted the post successfully!'
+    redirect_to user_path(current_user.id)
+    authorize! :destroy, @post
   end
 
   def post_params
